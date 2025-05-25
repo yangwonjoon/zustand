41f3ad2993034111f8fbbebdb9427dca48bf3d24
yang <diddnjswns20@gmail.com>
2025-05-25 09:48:53 +0900

    tictakto

--- src/app/tictak/page.tsx
+++ src/app/tictak/page.tsx
@@ -1,5 +1,11 @@
+import Board from "@/components/Board";
+
 const TicTak = () => {
-  return <div>TicTak</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Board />
+    </div>
+  );
 };
 
 export default TicTak;
